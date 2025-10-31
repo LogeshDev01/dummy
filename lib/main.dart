31711a2261568182441b379ce42f992d26d3f5c5
LogeshDev <logesh@alabtechnology.com>
2025-10-31 13:11:02 +0530

    version 3 notification add

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,7 @@ class MainClass{
     int add (int a , int b){
         return a+b ;
     }
+     int sub (int a , int b){
+        return a-b ;
+    }
 }
--- version.dart
+++ version.dart
@@ -1,4 +1,4 @@
 
 
 
-final version = 1.0.1
+final version = 1.0.2
